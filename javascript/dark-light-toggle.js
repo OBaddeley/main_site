f9cca995747d20899cd04a6634c1eeebe2dff7d6
Olivia Baddeley <OBaddeley@users.noreply.github.com>
2026-03-05 18:57:15 +1300

    Update light dark theme switch

--- javascript/dark-light-toggle.js
+++ javascript/dark-light-toggle.js
@@ -5,21 +5,26 @@ if(localStorage.getItem("theme")){
   if(localStorage.getItem("theme") == "dark"){
     var theme = "dark";
     body.classList.add(theme);
+    toggleButton.textContent = '☀';
   }
   else {
     var theme = "light";
     body.classList.add(theme);
+    toggleButton.textContent = '☾';
   }
 } else {
   body.classList.add('light');
+  toggleButton.textContent = '☾';
 }
 
 toggleButton.addEventListener('click', () => {
     if (body.classList.contains('light')) {
         body.classList.replace('light', 'dark');
         localStorage.setItem('theme', 'dark');
+        toggleButton.textContent = '☀';
     } else {
         body.classList.replace('dark', 'light');
         localStorage.setItem('theme', 'light');
+        toggleButton.textContent = '☾';
     }
 });
